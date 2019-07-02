4c1fb9ef54f328cdf4162770af2fa2e5355e2271
wenyuanzhuo <775416111@qq.com>
2019-07-02 21:55:05 +0800

    docs

--- deepInJavaScript/proxy.js
+++ deepInJavaScript/proxy.js
@@ -1,15 +1,31 @@
-const proxy = new Proxy({}, {
-    get: function(target, property) {
-        return 35
-    }
-})
-console.log(proxy.time)
+// const proxy = new Proxy({}, {
+//     get: function(target, property) {
+//         return 35
+//     }
+// })
+// console.log(proxy.time)
 
-function create1 (o) {//复制对象 不能访问o原型上的属性
-  function f() {}
-  f.prototype = o
-  return new f()
-}
-var c = create1(proxy)  
-console.log(c)
+// function create1 (o) {//复制对象 不能访问o原型上的属性
+//   function f() {}
+//   f.prototype = o
+//   return new f()
+// }
+// var c = create1(proxy)  
+// console.log(c)
 
+const arr = [{
+  type: '111',
+  id: 1
+}, {
+  id: 1
+}, {
+  type: '111',
+  id: 3
+}]
+// const bool = arr.filter(one => one.type)
+// .filter(item => item.type !== 'rewardAd').length > 0
+const bool = arr.reduce((result, curr) => {
+  if (curr.type && curr.type !== 'rewardAd') result++
+  return result
+}, 0)
+console.log(bool)
