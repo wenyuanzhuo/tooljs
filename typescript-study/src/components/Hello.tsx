5b0279588e90d92f89d1d68c4374bd0cca3a9c5d
wenyuanzhuo <775416111@qq.com>
2019-07-10 19:34:36 +0800

    [daily] ts two === the base type of interface

--- typescript-study/src/components/Hello.tsx
+++ typescript-study/src/components/Hello.tsx
@@ -9,11 +9,32 @@ export enum NameEnum {
   Guuka = 'guuka'
 }
 
+export interface DefaultObj {
+  age?: number;
+  address?: string;
+  readonly x?: number; // 只读
+}
+
+interface Named {
+  name: string;
+}
+
+class Person {
+  name: string;
+}
+
+let p: Named;
+// OK, because of structural typing
+p = new Person();
 function Hello ({name, enthusiasmLevel = 1}: Props) {
+  if (enthusiasmLevel <= 0) {
+    error('something failed')
+  }
   return (
     <div>
       <h1>我是{name}</h1>
       <span>{getExclamationMarks(enthusiasmLevel)}</span>
+      <h1>{getDefaultObj({x: 10})}</h1>
     </div>
   )
 }
@@ -21,5 +42,10 @@ function Hello ({name, enthusiasmLevel = 1}: Props) {
 function getExclamationMarks(numChars: number): string {
   return Array(numChars + 1).join('!')
 }
-
+function error(message: string): never {
+  throw new Error(message)
+}
+function getDefaultObj(obj: DefaultObj) {
+  return Object.keys(obj).join('')
+}
 export default Hello
