77cc3a397db1ddf8f0cc78a358a42d8015c7dd6e
wenyuanzhuo <775416111@qq.com>
2019-07-12 10:00:54 +0800

    [daily] ts four === the base type of interface function

--- typescript-study/src/components/Hello.tsx
+++ typescript-study/src/components/Hello.tsx
@@ -49,6 +49,49 @@ function error(message: string): never {
 function getDefaultObj(obj: DefaultObj) {
   return Object.keys(obj).join('')
 }
+// 接口
+// 描述普通对象
+// interface SquareConfig {
+//   color?: string;
+//   width?: number;
+//   // [propName: string]: any;
+// }
+// function createSquare(config: SquareConfig): { color: string; area: number } {
+//   return { color: config.color || '1', area: config.width || 0 }
+// }
+// // let mySquare = createSquare({ colour: "red", width: 100 } as SquareConfig);
+// let Squareoptions = { colour: "red", width: 100 }
+// let mySquare = createSquare(Squareoptions);
+// 要留意，在像上面一样的简单代码里，你可能不应该去绕开这些检查。 
+// 对于包含方法和内部状态的复杂对象字面量来讲，你可能需要使用这些技巧，但是大部额外属性检查错误是真正的bug。
+
+// 描述函数对象
+interface SearchFunc {
+  (source: string, subString: string): boolean;
+}
+
+// -----------------
+
+let mySearch: SearchFunc;
+mySearch = (src: string, subS: string): boolean => {
+  const result = src.search(subS)
+  console.log(result)
+  return result > -1
+}
+mySearch('www.123.com', '123')
+
+// -------------
+interface MyInterface {
+  // This is call signature
+  // It is used inside object type, function expression, function declaration, etc...
+   (x:number, y:number): number; 
+}
+
+const myOne : MyInterface = (x,y) => {
+  console.log(111)
+  return x + y
+};
+myOne(1, 2)
 // 泛型
 function identity<T>(arg: T): T {
   return arg
