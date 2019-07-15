cf73e7bb2da7302916af9179f1d7787cd8af0e39
wenyuanzhuo <775416111@qq.com>
2019-07-15 19:28:00 +0800

    [add] A+

--- deepInJavaScript/promise.js
+++ deepInJavaScript/promise.js
@@ -18,46 +18,46 @@
 // }
 // pro().then((v) => console.log(2, v))
 
-// function promise(fn) {
-//   var value
-//   var state = 'pending'
-//   var deferred = null
-//   this.then = function(onResolved) {
-//     return new promise(function (resolve) {
-//       handle({
-//         onResolved: onResolved,
-//         resolve: resolve
-//       })
-//     })
+function promise(fn) {
+  var value
+  var state = 'pending'
+  var deferred = null
+  this.then = function(onResolved) {
+    return new promise(function (resolve) {
+      handle({
+        onResolved: onResolved,
+        resolve: resolve
+      })
+    })
     
-//   }
-//   function handle(handler) {
-//     console.log('=====', state)
-//     if(state === 'pending') {
-//       deferred = handler
-//       return
-//     }
-//     if(!handler.onResolved) {
-//       handler.resolve(value)
-//       return
-//     }
-//     var newValue = handler.onResolved(value) //上一个then的返回值作为参数 这就是为什么一定要返回
-//     handler.resolve(newValue)
-//   }
-//   function resolve(newValue) { // 成功走的流程 state -> resolve
-//     if (newValue && typeof newValue.then === 'function') {// newValue是个promise走的逻辑
-//       console.log(222)
-//       newValue.then(resolve)
-//     }
-//     value = newValue
-//     console.log('11', newValue)
-//     state = 'resolved'
-//     if(deferred) {
-//       handle(deferred)
-//     }
-//   }
-//   fn(resolve)
-// }
+  }
+  function handle(handler) {
+    console.log('=====', state)
+    if(state === 'pending') {
+      deferred = handler
+      return
+    }
+    if(!handler.onResolved) {
+      handler.resolve(value)
+      return
+    }
+    var newValue = handler.onResolved(value) //上一个then的返回值作为参数 这就是为什么一定要返回
+    handler.resolve(newValue)
+  }
+  function resolve(newValue) { // 成功走的流程 state -> resolve
+    if (newValue && typeof newValue.then === 'function') {// newValue是个promise走的逻辑
+      console.log(222)
+      newValue.then(resolve)
+    }
+    value = newValue
+    console.log('11', newValue)
+    state = 'resolved'
+    if(deferred) {
+      handle(deferred)
+    }
+  }
+  fn(resolve)
+}
 // function doSomething() {
 //   return new promise(function (resolve) { // fn
 //     resolve(42)
@@ -74,9 +74,30 @@
 //   return 108
 // })
 
-
+function isPromiseThenablejob(newValue) {
+  if (newValue && typeof newValue.then === 'function') {// newValue是个promise走的逻辑
+    console.log(222)
+  }
+}
+function promiseTypeOf(val) {
+  console.log(typeof val)
+}
+function myTestIsPromiseThenablejob() {
+  return new Promise(function (resolve, reject) {
+    const promise = Promise.resolve(10)
+    isPromiseThenablejob(promise)
+    promiseTypeOf(promise)
+    resolve({})
+    reject({name: '11'})  // 2.3.3.3.3 
+  });
+}
+myTestIsPromiseThenablejob()
+  .then((res) => {
+    console.log(res)
+  })
+  .catch(err => console.log(err))
 async function doSomething() {
-  return new Promise(function (resolve) {
+  return new Promise(function (resolve, reject) {
     resolve(42)
   });
 }
@@ -84,7 +105,6 @@ async function p () {
   await Promise.all([doSomething()]).then((result) => {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-          console.log(2, result)
           resolve({
               name: "第2个传递的值"
           })
@@ -99,15 +119,27 @@ async function p () {
     console.log(3333)
   }, 0)
 }
-p()
+// p()
+
+
+
+
+
 // function p(fn) {
 //   let value
-//   this.then(() => {
-//     // return promise
-//     return new p((resolve) => {
+//   let state = 'pending'
+//   this.then = (onResolved) => {
 
+//     return new p(function () {
+//       handle({
+//         onResolved: onResolved,
+//         resolve: resolve
+//       })
 //     })
-//   })
+//   }
+//   function handle () {
+
+//   }
 //   function resolve(newValue) {
 //     console.log(newValue)
 //     value = newValue
