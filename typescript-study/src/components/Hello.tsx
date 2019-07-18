74a2be2040d64c2d99d3f8e66e4caf7de4788265
wenyuanzhuo <775416111@qq.com>
2019-07-18 20:06:23 +0800

    [daily] ts five === function

--- typescript-study/src/components/Hello.tsx
+++ typescript-study/src/components/Hello.tsx
@@ -217,6 +217,12 @@ export default Hello
 // createInstance(Lion).keeper.hasMask
 // createInstance(Bee).keeper.hasMask
 
+// --------------------------------------------------------------
+
+
+
+
+
 
 // 类
 // class Animal {
@@ -282,6 +288,7 @@ export default Hello
 // department = new AccountingDepartment(); // 允许对一个抽象子类进行实例化和赋值
 // department.printMeeting()
 
+// 静态属性
 // class Greeter {
 //   static standardGreeting = 'Aaron'; // 静态属性 只存在类本身  不存在实例上
 //   greet() {
@@ -293,3 +300,70 @@ export default Hello
 // greeterMaker.standardGreeting = 'nick'
 // const greeter: Greeter = new greeterMaker()
 // console.log(greeter.greet()) // nick
+
+// --------------------------------------------------------------
+
+
+// interface myNumber {
+//   (a: number, b?: number): number
+// }
+
+// // 函数
+// let myAdd: myNumber = function(x: number = 2, y: number = 1) {
+//   return x + y
+// }
+// myAdd(1, 2)
+
+// this参数
+// interface Card {
+//   suit: string;
+//   card: number;
+// }
+// interface Deck {
+//   suits: string[];
+//   cards: number[];
+//   createCardPicker(this: Deck): () => Card;
+// }
+// const deck: Deck = {
+//   suits: ["hearts", "spades", "clubs", "diamonds"],
+//   cards: Array(52),
+//   'createCardPicker'(this: Deck) {
+//       // NOTE: the line below is now an arrow function, allowing us to capture 'this' right here
+//       return () => {
+//           const pickedCard = Math.floor(Math.random() * 52);
+//           const pickedSuit = Math.floor(pickedCard / 13);
+
+//           return {suit: this.suits[pickedSuit], card: pickedCard % 13};
+//       }
+//   }
+// }
+
+// const cardPicker = deck.createCardPicker();
+// const pickedCards = cardPicker();
+
+// console.log("card: " + pickedCards.card + " of " + pickedCards.suit);
+
+// const suits = ["hearts", "spades", "clubs", "diamonds"];
+
+// function pickCard(x: {suit: string; card: number; }[]): number;
+// function pickCard(x: number): {suit: string; card: number; };
+// function pickCard(x: any): any {
+//     // Check to see if we're working with an object/array
+//     // if so, they gave us the deck and we'll pick the card
+//     if (typeof x == "object") {
+//         const pickedCard = Math.floor(Math.random() * x.length);
+//         return pickedCard;
+//     }
+//     // Otherwise just let them pick the card
+//     else if (typeof x == "number") {
+//         const pickedSuit = Math.floor(x / 13);
+//         return { suit: suits[pickedSuit], card: x % 13 };
+//     }
+// }
+
+// const myDeck = [{ suit: "diamonds", card: 2 }, { suit: "spades", card: 10 }, { suit: "hearts", card: 4 }];
+// const pickedCard1 = myDeck[pickCard(myDeck)];
+// alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
+
+// const pickedCard2 = pickCard(15);
+// alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
