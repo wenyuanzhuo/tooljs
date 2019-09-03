b557e703ffa0bc3e5d2cea00f5466a2a42cc08d5
wenyuanzhuo <775416111@qq.com>
2019-09-03 20:10:27 +0800

    [wip] redux

--- typescript-study/src/actions/actionsTypes.ts
+++
@@ -1,6 +0,0 @@
-
-
-
-export const fetchTodos = () => {
-  return 
-}
---
+++ typescript-study/src/actions/todos.ts
@@ -0,0 +1,34 @@
+import { Dispatch } from 'redux';
+import axios from 'axios';
+import { ActionTypes } from './actionTypes';
+import { Todo } from '../reducers/index';
+
+export interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
+export interface DeleteTodoAction {
+  type: ActionTypes.deleteTodo;
+  payload: number;
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch) => {
+    const response = await axios.get<Todo[]>(url); // shows that we're expecting an array of Todos implementing the Todo interface
+
+    dispatch<FetchTodosAction>({
+      type: ActionTypes.fetchTodos,
+      payload: response.data
+    });
+  };
+};
+
+export const deleteTodo = (id: number): DeleteTodoAction => {
+  return {
+    type: ActionTypes.deleteTodo,
+    payload: id
+  };
+};
