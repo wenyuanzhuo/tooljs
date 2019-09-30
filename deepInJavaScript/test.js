1c28b3fdc05deb31f1850c90df3773c4edb07592
wenyuanzhuo <775416111@qq.com>
2019-09-30 14:40:40 +0800

    [write] async

--- deepInJavaScript/test.js
+++ deepInJavaScript/test.js
@@ -1,15 +1,23 @@
 async function foo() {
   console.log('foo') // 3
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(1)
+    }, 1000)
+    // resolve(1)
+  })
 }
 async function bar() {
   console.log('bar start') // 2
-  await foo()
+  await foo().then(() => {
+    console.log('foo after then')
+  })
   console.log('bar end')  // 6
 }
 console.log('script start') // 1
-setTimeout(function () {
-  console.log('setTimeout') // 8
-}, 0)
+// setTimeout(function () {
+//   console.log('setTimeout') // 8
+// }, 0)
 bar();
 new Promise(function (resolve) {
   console.log('promise executor') // 4
@@ -17,4 +25,4 @@ new Promise(function (resolve) {
 }).then(function () {
   console.log('promise then')  // 7
 })
-console.log('script end') // 5
+// console.log('script end') // 5
