5190470c1687ab2274adb2672207572d9aa160ad
wenyuanzhuo <775416111@qq.com>
2019-11-10 20:54:45 +0800

    [arithmentic] recursion 2 bind new

--- deepInJavaScript/calldemo.js
+++ deepInJavaScript/calldemo.js
@@ -37,14 +37,14 @@ Function.prototype.bind2 = function (context) {
     // self.apply(context, args.concat(bindArg))
   }
   // fBound.prototype = this.prototype //原型链继承 fBound.prototype改变导致绑定函数原型也发生改变
-  fNOP.prototype = this.prototype // create函数继承 寄生继承？
+  fNOP.prototype = this.prototype // create函数继承
   fBound.prototype = new fNOP()
   return fBound
 }
-var res = bar.bind2(obj, 'Aaron', 'wen')//获取第二位开始的参数 
+// var res = bar.bind2(obj, 'Aaron', 'wen')//获取第二位开始的参数 
 // res('18')
-var f = res('18') //闭包执行环境 globa 闭包内this指向window   如果new res this指向 实例
-console.log(f)
+// var f = new res('18') //闭包执行环境 globa 闭包内this指向window   如果new res this指向 实例
+// console.log(f)
 /*
   var obj1 = {
     value: 2,
@@ -77,7 +77,6 @@ function newNext () {
   var dim = [].shift.call(arguments) //Otake arguments[0]
   obj.__proto__ = dim.prototype //访问Otake原型上的属性
   var answer = dim.call(obj, ...arguments) //改变this 访问Otake上的属性
-  console.log(answer)
   return typeof answer === 'object' ? answer :obj
   // return obj
 }
@@ -108,3 +107,64 @@ function myExtends (child, parent) {
 // myExtends(Child, Parent)
 // var a = new Child('child')
 // console.log(a)
+
+
+
+// 继承
+// function Parent () {
+//   this.names = ['kevin', 'daisy'];
+// }
+
+// function Child () {
+
+// }
+// Child.prototype = Parent.prototype
+// Child.prototype = new Parent();
+
+// var child1 = new Child();
+
+// child1.names.push('yayu');
+
+// console.log(child1.names); // ["kevin", "daisy", "yayu"]
+
+// var child2 = new Child();
+
+// console.log(child2.names); // ["kevin", "daisy", "yayu"]
+
+//--------------------------------------------------------
+function Parent () {
+  this.names = ['kevin', 'daisy'];
+}
+Parent.prototype.getName = function () {
+  console.log(this.names)
+}
+function Child () {
+  Parent.call(this); // 每次实例化都创建 一遍Parent上的方法 属性，同时原型也没有绑定
+}
+// 1. 污染父子原型 导致相互影响
+// Child.prototype = Parent.prototype
+
+// 2. 对于1的解决办法——创建个空函数，原型指向父函数的原型（作用：避免Call重复创建，同时避免父子函数原型指向同一个对象）
+
+// 换句话说 通过污染F函数的原型（F函数外作用域读取不到，所以无所谓，但是还是要占用内存的），c实例__proto__ => F.prototype => P.prtototype
+function create1 (_prototype_) {
+  function F () {}
+  F.prototype = _prototype_
+  return new F()
+}
+Child.prototype = create1(Parent.prototype)
+
+Child.prototype.childGetNames = function () {
+  console.log(11111)
+}
+var child1 = new Child();
+
+child1.names.push('yayu');
+child1.getName()
+child1.childGetNames()
+
+// console.log(child1.names); // ["kevin", "daisy", "yayu"]
+
+// var child2 = new Child();
+
+// console.log(child2.names); // ["kevin", "daisy"]
